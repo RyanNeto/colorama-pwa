2b91db87262102aa400441555fe3d9e67049bcc5
RyanNeto <74247888+RyanNeto@users.noreply.github.com>
2023-10-27 22:11:02 -0400

    segundo
    none

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -68,18 +68,7 @@ registerRoute(
     ],
   }))
 
-registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.js'),
-  // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: 'Javascript',
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 }),
-    ],
-  }))
+
 
 
 // This allows the web app to trigger skipWaiting via
